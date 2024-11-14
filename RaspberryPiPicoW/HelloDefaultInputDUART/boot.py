ee4637aacada24d32cf280c683ad9d63aca746ed
Eloi Strée <eloistree@gmail.com>
2024-11-14 06:22:55 +0100

    MAGIE MAGIE :)-

--- RaspberryPiPicoW/HelloDefaultInputDUART/boot.py
+++ RaspberryPiPicoW/HelloDefaultInputDUART/boot.py
@@ -1,65 +1,72 @@
 import usb_hid
 import usb_midi
+import supervisor
 import board
-
 import usb_cdc
 # Set the USB serial device name
-usb_cdc.console.serial = "LoveFrenchFries"
+#usb_cdc.console.serial = "LoveFrenchFries"
+import board
+import usb_cdc
+import time
+from adafruit_hid import find_device
+import usb_hid
+import usb_hid
 
-# This is only one example of a gamepad descriptor, and may not suit your needs.
+# IMPORTANT !!!!!!!!! IF YOU CHANGE YOU NEED A HARD RESET TO RECOGNIZE THE DEVICE
+# TURN OFF THE DEVICE THEN GO IN BOOT MODE THEN COME BACK TO NORMAL MODE
+# This is an updated example of a gamepad report descriptor with 32 buttons.
 GAMEPAD_REPORT_DESCRIPTOR = bytes((
-    0x05, 0x01,  # Usage Page (Generic Desktop Ctrls)
-    0x09, 0x05,  # Usage (Game Pad)
-    0xA1, 0x01,  # Collection (Application)
-    0x85, 0x04,  #   Report ID (4)
-    0x05, 0x09,  #   Usage Page (Button)
-    0x19, 0x01,  #   Usage Minimum (Button 1)
-    0x29, 0x10,  #   Usage Maximum (Button 16)
-    0x15, 0x00,  #   Logical Minimum (0)
-    0x25, 0x01,  #   Logical Maximum (1)
-    0x75, 0x01,  #   Report Size (1)
-    0x95, 0x10,  #   Report Count (16)
-    0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x05, 0x01,  #   Usage Page (Generic Desktop Ctrls)
-    0x15, 0x81,  #   Logical Minimum (-127)
-    0x25, 0x7F,  #   Logical Maximum (127)
-    0x09, 0x30,  #   Usage (X)
-    0x09, 0x31,  #   Usage (Y)
-    0x75, 0x08,  #   Report Size (8)
-    0x95, 0x02,  #   Report Count (4)
-    0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0x39,	 # USAGE (Hat switch) */
-    0x15, 0x01,	 # LOGICAL_MINIMUM (1) */
-    0x25, 0x08,	 # LOGICAL_MAXIMUM (8) */
-    0x95, 0x02,	 # REPORT_COUNT (2) */
-    0x75, 0x04,	 # REPORT_SIZE (4) */
-    0x25, 0x01,	 #  INPUT (Data,Var,Abs) */
-    0xC0,        # End Collection
+    0x05, 0x01,        # Usage Page (Generic Desktop Ctrls)
+    0x09, 0x05,        # Usage (Game Pad)
+    0xA1, 0x01,        # Collection (Application)
+    0x85, 0x04,        #   Report ID (4)
+    
+    # Buttons (32 buttons)
+    0x05, 0x09,        #   Usage Page (Button)
+    0x19, 0x01,        #   Usage Minimum (Button 1)
+    0x29, 0x20,        #   Usage Maximum (Button 32)
+    0x15, 0x00,        #   Logical Minimum (0)
+    0x25, 0x01,        #   Logical Maximum (1)
+    0x75, 0x01,        #   Report Size (1)
+    0x95, 0x20,        #   Report Count (32)
+    0x81, 0x02,        #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    
+    # Axes (X, Y, Z, Rz)
+    0x05, 0x01,        #   Usage Page (Generic Desktop Ctrls)
+    0x15, 0x81,        #   Logical Minimum (-127)
+    0x25, 0x7F,        #   Logical Maximum (127)
+    0x09, 0x30,        #   Usage (X)
+    0x09, 0x31,        #   Usage (Y)
+    0x09, 0x32,        #   Usage (Z)
+    0x09, 0x35,        #   Usage (Rz)
+    0x75, 0x08,        #   Report Size (8)
+    0x95, 0x04,        #   Report Count (4)
+    0x81, 0x02,        #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    
+    # Triggers (Rx, Ry)
+    0x09, 0x33,        #   Usage (Rx) - Left Trigger
+    0x09, 0x34,        #   Usage (Ry) - Right Trigger
+    0x15, 0x00,        #   Logical Minimum (0)
+    0x25, 0xFF,        #   Logical Maximum (255)
+    0x75, 0x08,        #   Report Size (8)
+    0x95, 0x02,        #   Report Count (2)
+    0x81, 0x02,        #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    
+    0xC0               # End Collection
 ))
-
 
 gamepad = usb_hid.Device(
     report_descriptor=GAMEPAD_REPORT_DESCRIPTOR,
     usage_page=0x01,           # Generic Desktop Control
     usage=0x05,                # Gamepad
     report_ids=(4,),           # Descriptor uses report ID 4.
-    in_report_lengths=(6,),    # This gamepad sends 6 bytes in its report.
+    in_report_lengths=(10,),    # This gamepad sends 6 bytes in its report.
     out_report_lengths=(0,),   # It does not receive any reports.
 )
-
 usb_hid.enable(
     (usb_hid.Device.KEYBOARD,
      usb_hid.Device.MOUSE,
      usb_hid.Device.CONSUMER_CONTROL,
-     gamepad,
-     vendor_id=0x2501,  # Replace with your desired Vendor ID
-     product_id=0x0042,  # Replace with your desired Product ID
-     manufacturer_string="eLab Factory",
-     product_string="eLab Midi Keyboard")
+     gamepad)
 )
-)
-
-usb_midi.enable(
-    vendor_id=2501,  # Replace with your desired Vendor ID
-    product_id=0x0314   # Replace with your desired Product ID
-)
+usb_midi.enable()
--- RaspberryPiPicoW/HelloDefaultInputDUART/hid_gamepad.py
+++ RaspberryPiPicoW/HelloDefaultInputDUART/hid_gamepad.py
@@ -1,8 +1,3 @@
-https://github.com/printnplay/PicoCader/blob/main/hid_gamepad.py
-# SPDX-FileCopyrightText: 2018 Dan Halbert for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
 """
 `Gamepad`
 ====================================================
@@ -17,14 +12,15 @@ from adafruit_hid import find_device
 
 
 class Gamepad:
-    """Emulate a generic gamepad controller with 16 buttons,
-    numbered 1-16, and two joysticks, one controlling
-    ``x` and ``y`` values, and the other controlling ``z`` and
+    """Emulate a generic gamepad controller with 32 buttons,
+    numbered 1-32, and two joysticks, one controlling
+    ``x`` and ``y`` values, and the other controlling ``z`` and
     ``r_z`` (z rotation or ``Rz``) values.
 
-    The joystick values could be interpreted
-    differently by the receiving program: those are just the names used here.
-    The joystick values are in the range -127 to 127."""
+    Two additional triggers provide ``rx`` and ``ry`` values.
+    The joystick values are in the range -127 to 127, and the trigger values
+    are in the range 0 to 255.
+    """
 
     def __init__(self, devices):
         """Create a Gamepad object that will send USB gamepad HID reports.
@@ -33,32 +29,29 @@ class Gamepad:
         itself. A device is any object that implements ``send_report()``, ``usage_page`` and
         ``usage``.
         """
-        self._gamepad_device = find_device(devices, usage_page=0x1, usage=0x05)
+        self._gamepad_device = find_device(devices, usage_page=0x01, usage=0x05)
+        
+        # Report layout:
+        # report[0-3] - buttons 1-32 (32 bits)
+        # report[4] - joystick 0 x: -127 to 127
+        # report[5] - joystick 0 y: -127 to 127
+        # report[6] - joystick 1 x: -127 to 127
+        # report[7] - joystick 1 y: -127 to 127
+        # report[8] - trigger rx: 0 to 255
+        # report[9] - trigger ry: 0 to 255
+        self._report = bytearray(10)
+        self._last_report = bytearray(10)
 
-        # Reuse this bytearray to send mouse reports.
-        # Typically controllers start numbering buttons at 1 rather than 0.
-        # report[0] buttons 1-8 (LSB is button 1)
-        # report[1] buttons 9-16
-        # report[2] joystick 0 x: -127 to 127
-        # report[3] joystick 0 y: -127 to 127
-        # report[4] joystick 1 x: -127 to 127
-        # report[5] joystick 1 y: -127 to 127
-        self._report = bytearray(6)
-
-        # Remember the last report as well, so we can avoid sending
-        # duplicate reports.
-        self._last_report = bytearray(6)
-
-        # Store settings separately before putting into report. Saves code
-        # especially for buttons.
+        # Store settings separately before putting into report.
         self._buttons_state = 0
         self._joy_x = 0
         self._joy_y = 0
         self._joy_z = 0
         self._joy_r_z = 0
+        self._joy_rx = 0
+        self._joy_ry = 0
 
         # Send an initial report to test if HID device is ready.
-        # If not, wait a bit and try once more.
         try:
             self.reset_all()
         except OSError:
@@ -68,18 +61,17 @@ class Gamepad:
     def press_buttons(self, *buttons):
         """Press and hold the given buttons."""
         for button in buttons:
-            self._buttons_state |= 1 << self._validate_button_number(button) - 1
+            self._buttons_state |= 1 << (self._validate_button_number(button) - 1)
         self._send()
 
     def release_buttons(self, *buttons):
         """Release the given buttons."""
         for button in buttons:
-            self._buttons_state &= ~(1 << self._validate_button_number(button) - 1)
+            self._buttons_state &= ~(1 << (self._validate_button_number(button) - 1))
         self._send()
 
     def release_all_buttons(self):
         """Release all the buttons."""
-
         self._buttons_state = 0
         self._send()
 
@@ -88,23 +80,63 @@ class Gamepad:
         self.press_buttons(*buttons)
         self.release_buttons(*buttons)
 
-    def move_joysticks(self, x=None, y=None, z=None, r_z=None):
-        """Set and send the given joystick values.
-        The joysticks will remain set with the given values until changed
+    def set_joystick_left_x_127(self, value):
+        """Set the x value of the left joystick."""
+        self.move_joysticks(x=value)
+    def set_joystick_left_y_127(self, value):
+        """Set the y value of the left joystick."""
+        self.move_joysticks(y=value)
+    def set_joystick_right_x_127(self, value):
+        """Set the x value of the right joystick."""
+        self.move_joysticks(z=value)
+    def set_joystick_right_y_127(self, value):
+        """Set the y value of the right joystick."""
+        self.move_joysticks(r_z=value)
+    def set_trigger_left_255(self, value):
+        """Set the left trigger value."""
+        self.move_joysticks(rx=value)
+    def set_trigger_right_255(self, value):
+        """Set the right trigger value."""
+        self.move_joysticks(ry=value)
+        
+    def set_joystick_left_x_percent(self, value):
+        """Set the x value of the left joystick."""
+        self.set_joystick_left_x_127(int(value * 127))
+    def set_joystick_left_y_percent(self, value):
+        """Set the y value of the left joystick."""
+        self.set_joystick_left_y_127(int(value * 127))
+    def set_joystick_right_x_percent(self, value):
+        """Set the x value of the right joystick."""
+        self.set_joystick_right_x_127(int(value * 127))
+    def set_joystick_right_y_percent(self, value):
+        """Set the y value of the right joystick."""
+        self.set_joystick_right_y_127(int(value * 127))
+    def set_trigger_left_percent(self, value):
+        """Set the left trigger value."""
+        self.set_trigger_left_255(int(value * 255))
+    def set_trigger_right_percent(self, value):
+        """Set the right trigger value."""
+        self.set_trigger_right_255(int(value * 255))
+        
+
+    def move_joysticks(self, x=None, y=None, z=None, r_z=None, rx=None, ry=None):
+        """Set and send the given joystick and trigger values.
+        The joysticks will remain set with the given values until changed.
 
         One joystick provides ``x`` and ``y`` values,
         and the other provides ``z`` and ``r_z`` (z rotation).
-        Any values left as ``None`` will not be changed.
+        Two additional triggers provide ``rx`` and ``ry`` values.
 
-        All values must be in the range -127 to 127 inclusive.
+        Joystick values must be in the range -127 to 127 inclusive.
+        Trigger values must be in the range 0 to 255 inclusive.
 
         Examples::
 
             # Change x and y values only.
             gp.move_joysticks(x=100, y=-50)
 
-            # Reset all joystick values to center position.
-            gp.move_joysticks(0, 0, 0, 0)
+            # Reset all joystick and trigger values to center position.
+            gp.move_joysticks(0, 0, 0, 0, 128, 128)
         """
         if x is not None:
             self._joy_x = self._validate_joystick_value(x)
@@ -114,15 +146,21 @@ class Gamepad:
             self._joy_z = self._validate_joystick_value(z)
         if r_z is not None:
             self._joy_r_z = self._validate_joystick_value(r_z)
+        if rx is not None:
+            self._joy_rx = self._validate_trigger_value(rx)
+        if ry is not None:
+            self._joy_ry = self._validate_trigger_value(ry)
         self._send()
 
     def reset_all(self):
-        """Release all buttons and set joysticks to zero."""
+        """Release all buttons and set joysticks and triggers to default positions."""
         self._buttons_state = 0
         self._joy_x = 0
         self._joy_y = 0
         self._joy_z = 0
         self._joy_r_z = 0
+        self._joy_rx = 0
+        self._joy_ry = 0
         self._send(always=True)
 
     def _send(self, always=False):
@@ -130,7 +168,7 @@ class Gamepad:
         If ``always`` is ``False`` (the default), send only if there have been changes.
         """
         struct.pack_into(
-            "<Hbbbb",
+            "<IbbbbBB",  # 'I' for 32-bit button state, followed by joystick and trigger values
             self._report,
             0,
             self._buttons_state,
@@ -138,21 +176,28 @@ class Gamepad:
             self._joy_y,
             self._joy_z,
             self._joy_r_z,
+            self._joy_rx,
+            self._joy_ry,
         )
 
         if always or self._last_report != self._report:
             self._gamepad_device.send_report(self._report)
-            # Remember what we sent, without allocating new storage.
             self._last_report[:] = self._report
 
     @staticmethod
     def _validate_button_number(button):
-        if not 1 <= button <= 16:
-            raise ValueError("Button number must in range 1 to 16")
+        if not 1 <= button <= 32:
+            raise ValueError("Button number must be in range 1 to 32")
         return button
 
     @staticmethod
     def _validate_joystick_value(value):
         if not -127 <= value <= 127:
             raise ValueError("Joystick value must be in range -127 to 127")
+        return value
+
+    @staticmethod
+    def _validate_trigger_value(value):
+        if not 0 <= value <= 255:
+            raise ValueError("Trigger value must be in range 0 to 255")
         return value
